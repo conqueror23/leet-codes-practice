5914067846acee267dfd37a48de417e8b085d88c
Bolong.wang <wayneborong@gmail.com>
2025-07-12 14:34:22 +1000

    feat: add draft solution

--- src/37-sudoku-solver.ts
+++ src/37-sudoku-solver.ts
@@ -18,21 +18,6 @@ The '.' character indicates empty cells.
   [".", ".", ".", "4", "1", "9", ".", ".", "5"],
   [".", ".", ".", ".", "8", ".", ".", "7", "9"]
   ]
-
-[
-  ["5", "3", "(0-9)", "(0-9)", "7", "(0-9)", "(0-9)", "(0-9)", "(0-9)"],
-  ["6", "(0-9)", "(0-9)", "1", "9", "5", "(0-9)", "(0-9)", "(0-9)"],
-  [".", "9", "8", ".", ".", ".", ".", "6", "."],
-  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-  [".", "6", ".", ".", ".", ".", "2", "8", "."],
-  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-  [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-  ]
-
-
-
 **/
 
 
@@ -48,43 +33,99 @@ const board1 = [
   [".", ".", ".", ".", "8", ".", ".", "7", "9"]
 ]
 
-// const export1 = [["5", "3", "4", "6", "7", "8", "9", "1", "2"], ["6", "7", "2", "1", "9", "5", "3", "4", "8"], ["1", "9", "8", "3", "4", "2", "5", "6", "7"], ["8", "5", "9", "7", "6", "1", "4", "2", "3"], ["4", "2", "6", "8", "5", "3", "7", "9", "1"], ["7", "1", "3", "9", "2", "4", "8", "5", "6"], ["9", "6", "1", "5", "3", "7", "2", "8", "4"], ["2", "8", "7", "4", "1", "9", "6", "3", "5"], ["3", "4", "5", "2", "8", "6", "1", "7", "9"]]
+const getBlockIndex = (x: number, y: number) => {
+  const reminder = Math.floor((x / 3))
+  const dividend = Math.floor((y / 3))
 
-// console.log("export1", export1)
+  const blockIndex = reminder + 3 * dividend
+  return blockIndex
+}
+
+
+const getLatestBoardRef = (board: string[][]) => {
+  const rowsRef: string[] = Array.from({ length: 9 }, () => "")
+  const columnsRef: string[] = Array.from({ length: 9 }, () => "")
+  const blocksRef: string[] = Array.from({ length: 9 }, () => "")
+
+  // console.log(`rows : ${rows}`)
+  // console.log(`columns : ${columns}`)
+  // console.log(`blocks : ${blocks}`)
+
+  board.forEach((row, rowIndex) => {
+    rowsRef[rowIndex] = row.filter(item => item != '.').join("")
+    // console.log("rows", rows)
+
+    row.forEach((column, columnIndex) => {
+      if (column !== '.') {
+        if (columnsRef[columnIndex].includes(column)) return
+        columnsRef[columnIndex] += column
+
+        const blockIndex = getBlockIndex(rowIndex, columnIndex)
+        if (blocksRef.includes(column)) return
+        blocksRef[blockIndex] += column
+      }
+    })
+  })
+
+  return { rowsRef, columnsRef, blocksRef }
+}
+
+const getAvailableNumbers = (
+  rowRef: string, columnsRef: string, blockRef: string
+): string[] => {
+  const numBase = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  const potentialNumbers = numBase
+    .filter((num) => !rowRef.includes(num))
+    .filter((num) => !columnsRef.includes(num))
+    .filter((num) => !blockRef.includes(num))
+
+  if (!potentialNumbers) {
+    console.log(`rowRef ${rowRef}`)
+    console.log(`columnRef ${columnsRef}`)
+    console.log(`block ${blockRef}`)
+  }
+  return potentialNumbers
+}
+
 function solveSudoku1(board: string[][]): string[][] {
+  // const recordsSet = new Map<number, Set<number | null>>()
 
-  const recordsSet = new Map<number, Set<number | null>>()
-  const base = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+  const { rowsRef, columnsRef, blocksRef } = getLatestBoardRef(board)
 
-  return board.map(
+  // console.log("rowsRef", rowsRef)
+  // console.log("columnsRef", columnsRef)
+  // console.log("blocksRef", blocksRef)
+
+  const potentialBoard = board.map(
     (row, rowIndex) => {
-
-      recordsSet.set(rowIndex, new Set())
       return row.map(
-        (cell, cellIndex) => {
+        (column, columnIndex) => {
+          if (column === '.') {
+            const blockIndex = getBlockIndex(rowIndex, columnIndex)
+            const rowRef = rowsRef[rowIndex]
+            const columnRef = columnsRef[columnIndex]
+            const blockRef = blocksRef[blockIndex]
+            const availableNumbers = getAvailableNumbers(rowRef, columnRef, blockRef)
+            const selectedNumber = availableNumbers[0]
+            rowsRef[rowIndex] += selectedNumber
+            columnsRef[columnIndex] += selectedNumber
+            blocksRef[blockIndex] += selectedNumber
 
-          const existingSet = recordsSet.get(rowIndex)
-          if (existingSet && existingSet.size > 0) {
-            if (existingSet.has(Number(cell)) {
-              //save delta of 
-              //
-
-            }
-
+            return selectedNumber
           }
+          return column
+        })
+    })
 
-          return cell
-        }
-
-      )
-    
-  )
-};
-
+  return potentialBoard
+}
 
 const input = board1
-console.log("solveSudoku1", solveSudoku1(input))
+// solveSudoku1(input)
 
+console.log("solveSudoku1", solveSudoku1(input))
+//
 //Testings 
 const isEqualArray = (
   arr1: (number | string)[][], arr2: (number | string)[][]) => {
@@ -93,3 +134,15 @@ const isEqualArray = (
 
 // console.log('eq1', isEqualArray([[1, 2, 3], [3, 2, 2]], [[1, 2, 3], [3, 2, 2]]))
 // console.log('eq2', isEqualArray([[1, 2, 5], [3, 2, 2]], [[1, 2, 3], [3, 2, 2]]))
+//
+/** [ '5', '3', '1', '2', '7', '2', '1', '1', '2' ],
+    [ '6', '2', '2', '1', '9', '5', '3', '2', '2' ],
+    [ '1', '9', '8', '2', '3', '2', '1', '6', '2' ],
+    [ '8', '1', '1', '5', '6', '1', '4', '2', '3' ],
+    [ '4', '2', '2', '8', '5', '3', '5', '2', '1' ],
+    [ '7', '1', '1', '3', '2', '1', '4', '4', '6' ],
+    [ '1', '6', '1', '3', '3', '7', '2', '8', '4' ],
+    [ '2', '2', '2', '4', '1', '9', '3', '3', '5' ],
+    [ '1', '1', '1', '2', '8', '2', '1', '7', '9' ]
+]
+**/
