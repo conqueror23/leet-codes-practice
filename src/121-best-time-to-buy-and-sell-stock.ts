2339927f3aef232a9a1ac19a746f2e03e80f97ac
conqueror23 <wayneborong@gmail.com>
2026-06-21 23:23:59 +1000

    feat: finish 121

---
+++ src/121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,19 @@
+function maxProfit(prices: number[]): number {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    minPrice = Math.min(minPrice, prices[i]);
+  }
+
+  return maxProfit;
+};
+
+
+const numbers = [7,1,5,3,6,4]
+
+const profit = maxProfit(numbers)
+// 5
+
+console.log(profit)
