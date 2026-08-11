9fed2321bd92541340121c0235fe198e59cb4ec2
conqueror23 <wayneborong@gmail.com>
2026-08-11 20:56:06 +1000

    feat: add getting all sub string solution

--- src/hash_table/560-subarray-sum-equals-k.ts
+++ src/hash_table/560-subarray-sum-equals-k.ts
@@ -15,7 +15,6 @@ function subarraySumB(nums: number[], k: number): number {
       }
     }
   }
-
   return count
 };
 
@@ -28,18 +27,64 @@ function subarraySumPre(nums: number[], k: number): number {
 
   for (const num of nums) {
     prefixSum += num
-    //0
+
     count += prefixCount.get(prefixSum - k) ?? 0
 
     prefixCount.set(
       prefixSum,
       (prefixCount.get(prefixSum) ?? 0) + 1
     )
-    console.log('num,prcount,sum,count', num, prefixCount, prefixSum, count);
   }
 
   return count
 }
+
+function subarraySumAll(nums: number[], k: number): number[][] {
+  let prefixSum = 0
+  const result: number[][] = []
+
+  const boundaries = new Map<number, number[]>()
+  boundaries.set(0, [0])
+
+  for (let end = 0; end < nums.length; end++) {
+    prefixSum += nums[end - 1]
+
+    const starts = boundaries.get(prefixSum - k) ?? []
+
+    for (const previousEnd of starts) {
+      result.push([previousEnd, end - 1])
+    }
+
+    const current = boundaries.get(prefixSum) ?? []
+    current.push(end)
+    boundaries.set(prefixSum, current)
+  }
+
+  return result
+}
+
+function subarraySumAll2(nums: number[], k: number): number[][] {
+  let prefixSum = 0
+  const results: Array<[number, number]> = []
+  const boundaries = new Map<number, number[]>([[0, [0]]])
+
+  for (let endBoundary = 1; endBoundary <= nums.length; endBoundary++) {
+    prefixSum += nums[endBoundary - 1]
+
+    console.log("",)
+
+    for (const startBoundary of boundaries.get(prefixSum - k) ?? []) {
+      results.push([startBoundary, endBoundary - 1])
+    }
+
+    const positions = boundaries.get(prefixSum) ?? []
+    positions.push(endBoundary)
+    boundaries.set(prefixSum, positions)
+  }
+
+  return results
+}
+
 const nums1 = [1, 1, 1], k1 = 2
 const res1 = 2
 
@@ -61,9 +106,9 @@ const res4 = 6
   //
   // check(`case 2`, subarraySum(nums2, k2), res2)
   //
-  check(`case 3`, subarraySumPre(nums3, k3), res3)
+  // check(`case 3`, subarraySumAll(nums3, k3), res3)
 
-  check(`case 4`, subarraySumPre(nums4, k4), res4)
+  check(`case 4`, subarraySumAll2(nums4, k4), res4)
   // const end = performance.now();
 
   // console.log(`Run time: ${end - start} ms`);
